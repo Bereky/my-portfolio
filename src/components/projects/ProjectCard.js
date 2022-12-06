ad63ce858092c6fe737032f393fe960a94d8a65b
bereky <bereketlemma4@gmail.com>
2022-12-06 23:20:04 +0300

    Add tooltip to icons

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,5 +1,6 @@
 import LaunchIcon from '@mui/icons-material/Launch';
 import CodeIcon from '@mui/icons-material/Code';
+import Tooltip from '@mui/material/Tooltip'
 
 const ProjectCard = (props) => {
     return ( 
@@ -18,28 +19,41 @@ const ProjectCard = (props) => {
                     </div> 
 
                     <div className="w-full h-12 flex justify-start items-start flex-row relative py-1">
-                        <div className="w-11 h-11 flex justify-center items-center absolute bg-almostWhite rounded-full shadow-md p-1">
+                        <Tooltip title={props.data.technologies[0].name}>
+                            <div className="w-11 h-11 flex justify-center items-center absolute bg-almostWhite rounded-full shadow-md p-1">
                                 <img src={props.data.technologies[0].src} alt={props.data.technologies[0].name} className="w-8"/>
-                        </div> 
-                        <div className="w-11 h-11 flex justify-center items-center absolute bg-almostWhite rounded-full left-9 shadow-md p-1">
-                            <img src={props.data.technologies[1].src} alt={props.data.technologies[1].name} className="w-8"/>
-                        </div> 
-                        <div className="w-11 h-11 flex justify-center items-center absolute bg-almostWhite rounded-full left-16 ml-2 shadow-md p-1">
-                            <img src={props.data.technologies[2].src} alt={props.data.technologies[2].name} className="w-8"/>
-                        </div>
+                            </div> 
+                        </Tooltip>
+                        
+                        <Tooltip title={props.data.technologies[1].name}>
+                            <div className="w-11 h-11 flex justify-center items-center absolute bg-almostWhite rounded-full left-9 shadow-md p-1">
+                                <img src={props.data.technologies[1].src} alt={props.data.technologies[1].name} className="w-8"/>
+                            </div> 
+                        </Tooltip>
+                        <Tooltip title={props.data.technologies[2].name}>
+                            <div className="w-11 h-11 flex justify-center items-center absolute bg-almostWhite rounded-full left-16 ml-2 shadow-md p-1">
+                                <img src={props.data.technologies[2].src} alt={props.data.technologies[2].name} className="w-8"/>
+                            </div>
+                        </Tooltip>
+
                     </div>
 
                     <div className="w-full h-10 flex justify-end items-end flex-row my-1 ">
-                        <div className="w-9 h-9 flex justify-center items-center bg-almostWhite rounded-full shadow-md cursor-pointer hover:bg-inherit duration-300 mx-1">
-                            <a href={props.data.sourceCode} target="_blank" rel="noreferrer">
-                                <CodeIcon />
-                            </a> 
-                        </div> 
-                        <div className="w-9 h-9 flex justify-center items-center bg-almostWhite rounded-full shadow-md cursor-pointer hover:bg-inherit duration-300 mx-1">
-                            <a href={props.data.externalLink} target="_blank" rel="noreferrer">
-                                <LaunchIcon />
-                            </a>
-                        </div>
+                        <Tooltip title="Source code">
+                            <div className="w-9 h-9 flex justify-center items-center bg-almostWhite rounded-full shadow-md cursor-pointer hover:bg-inherit duration-300 mx-1">
+                                <a href={props.data.sourceCode} target="_blank" rel="noreferrer">
+                                    <CodeIcon />
+                                </a> 
+                            </div> 
+                        </Tooltip>
+
+                        <Tooltip title="Preview site">
+                            <div className="w-9 h-9 flex justify-center items-center bg-almostWhite rounded-full shadow-md cursor-pointer hover:bg-inherit duration-300 mx-1">
+                                <a href={props.data.externalLink} target="_blank" rel="noreferrer">
+                                    <LaunchIcon />
+                                </a>
+                            </div>
+                        </Tooltip>
                     </div>
                 </div>
             </div>
