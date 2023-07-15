2d6d490a0593aca46c8d8bbdabeeb361f0c8eb41
unknown <bereketlemma4@gmail.com>
2023-07-14 16:50:02 -0900

    added config for new projects

--- src/components/projects/projectConfig.js
+++ src/components/projects/projectConfig.js
@@ -1,77 +1,187 @@
-import dashImage from '../../asset/dash.png'
-import decentral from '../../asset/d3.png'
-import gfind from '../../asset/gfind.png'
-import shema from '../../asset/shema.png'
-import keyboard from '../../asset/keyboard.png'
-import pwdgenerator from '../../asset/pgen.png'
+import dashImage from "../../asset/dash.png";
+import decentral from "../../asset/d3.png";
+import gfind from "../../asset/gfind.png";
+import shema from "../../asset/shema.png";
+import keyboard from "../../asset/keyboard.png";
+import pwdgenerator from "../../asset/pgen.png";
+import queueEase from "../../asset/queue.png";
+import chapa from "../../asset/chapa.png";
+import fusion from "../../asset/fusion.png";
 
 const projectConfig = [
-    {
-        name: 'Dash Donation',
-        imageSrc: dashImage,
-        technologies: [
-            {name: 'React', src: 'https://cdn-icons-png.flaticon.com/512/3334/3334886.png'},
-            {name: 'Tailwind CSS', src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png'},
-            {name: 'Material UI', src: 'https://v4.mui.com/static/logo.png'},
-        ],
-        sourceCode: 'https://github.com/Bereky/dash_donation',
-        externalLink: 'https://dash-donation.vercel.app/'
-    },
-    {
-        name: 'Shema',
-        imageSrc: shema,
-        technologies: [
-            {name: 'React', src: 'https://cdn-icons-png.flaticon.com/512/3334/3334886.png'},
-            {name: 'ExpressJS', src: 'https://tw-strapi.s3.amazonaws.com/expressjs_ar21_b4dd392c7c.svg'},
-            {name: 'MongoDB', src: 'https://www.svgrepo.com/show/331488/mongodb.svg'},
-        ],
-        sourceCode: 'https://github.com/Bereky/mern-ecommerce',
-        externalLink: 'https://shemashop.vercel.app/'
-    },
-    {
-        name: 'Decentral',
-        imageSrc: decentral,
-        technologies: [
-            {name: 'React', src: 'https://cdn-icons-png.flaticon.com/512/3334/3334886.png'},
-            {name: 'Tailwind CSS', src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png'},
-            {name: 'Material UI', src: 'https://v4.mui.com/static/logo.png'},
-        ],
-        sourceCode: 'https://github.com/Bereky/easy_UIs/tree/main/one',
-        externalLink: 'https://decentral-gray.vercel.app/'
-    },
-    {
-        name: 'Github Find',
-        imageSrc: gfind,
-        technologies: [
-            {name: 'React', src: 'https://cdn-icons-png.flaticon.com/512/3334/3334886.png'},
-            {name: 'CSS', src: 'https://cdn-icons-png.flaticon.com/512/732/732190.png'},
-            {name: 'Material UI', src: 'https://v4.mui.com/static/logo.png'},
-        ],
-        sourceCode: 'https://github.com/Bereky/Github-find',
-        externalLink: 'https://eclectic-hamster-b6841d.netlify.app/'
-    },
-    {
-        name: 'Password Generator',
-        imageSrc: pwdgenerator,
-        technologies: [
-            {name: 'React', src: 'https://cdn-icons-png.flaticon.com/512/3334/3334886.png'},
-            {name: 'Tailwind CSS', src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png'},
-            {name: 'Redux', src: 'https://img.icons8.com/color/512/redux.png'},
-        ],
-        sourceCode: 'https://github.com/Bereky/password-generator',
-        externalLink: 'https://generate-pwd.vercel.app/'
-    },
-    {
-        name: 'Amharic Keyboard',
-        imageSrc: keyboard,
-        technologies: [
-            {name: 'HTML', src: 'https://cdn-icons-png.flaticon.com/512/174/174854.png'},
-            {name: 'CSS', src: 'https://cdn-icons-png.flaticon.com/512/732/732190.png'},
-            {name: 'Bootstrap', src: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg'}
-        ],
-        sourceCode: 'https://github.com/Bereky/Amharic-keyboard',
-        externalLink: 'https://remarkable-dodol-8876e4.netlify.app/'
-    }
-]
- 
+  {
+    name: "360FUSION - Virtual Tour Creator",
+    imageSrc: fusion,
+    technologies: [
+      {
+        name: "React",
+        src: "https://cdn-icons-png.flaticon.com/512/3334/3334886.png",
+      },
+      {
+        name: "ExpressJS",
+        src: "https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png",
+      },
+      {
+        name: "MongoDB",
+        src: "https://www.svgrepo.com/show/331488/mongodb.svg",
+      },
+    ],
+    sourceCode: "https://github.com/Bereky/dash_donation",
+    externalLink: "https://dash-donation.vercel.app/",
+  },
+
+  {
+    name: "QueueEase",
+    imageSrc: queueEase,
+    technologies: [
+      {
+        name: "React",
+        src: "https://cdn-icons-png.flaticon.com/512/3334/3334886.png",
+      },
+      {
+        name: "ExpressJS",
+        src: "https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png",
+      },
+      {
+        name: "MongoDB",
+        src: "https://www.svgrepo.com/show/331488/mongodb.svg",
+      },
+    ],
+    sourceCode: "https://github.com/Bereky/dash_donation",
+    externalLink: "https://dash-donation.vercel.app/",
+  },
+
+  {
+    name: "Chapa Nodejs Boilerplate",
+    imageSrc: chapa,
+    technologies: [
+      {
+        name: "NodeJS",
+        src: "https://cdn-icons-png.flaticon.com/512/5968/5968322.png",
+      },
+
+      {
+        name: "ExpressJS",
+        src: "https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png",
+      },
+
+      {
+        name: "Ejs",
+        src: "https://creazilla-store.fra1.digitaloceanspaces.com/icons/3243967/ejs-icon-md.png",
+      },
+    ],
+    sourceCode: "https://github.com/Bereky/dash_donation",
+    externalLink: "https://dash-donation.vercel.app/",
+  },
+
+  {
+    name: "Dash Donation",
+    imageSrc: dashImage,
+    technologies: [
+      {
+        name: "React",
+        src: "https://cdn-icons-png.flaticon.com/512/3334/3334886.png",
+      },
+      {
+        name: "Tailwind CSS",
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png",
+      },
+      { name: "Material UI", src: "https://v4.mui.com/static/logo.png" },
+    ],
+    sourceCode: "https://github.com/Bereky/dash_donation",
+    externalLink: "https://dash-donation.vercel.app/",
+  },
+  {
+    name: "Shema",
+    imageSrc: shema,
+    technologies: [
+      {
+        name: "React",
+        src: "https://cdn-icons-png.flaticon.com/512/3334/3334886.png",
+      },
+      {
+        name: "ExpressJS",
+        src: "https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png",
+      },
+      {
+        name: "MongoDB",
+        src: "https://www.svgrepo.com/show/331488/mongodb.svg",
+      },
+    ],
+    sourceCode: "https://github.com/Bereky/mern-ecommerce",
+    externalLink: "https://shemashop.vercel.app/",
+  },
+  {
+    name: "Decentral",
+    imageSrc: decentral,
+    technologies: [
+      {
+        name: "React",
+        src: "https://cdn-icons-png.flaticon.com/512/3334/3334886.png",
+      },
+      {
+        name: "Tailwind CSS",
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png",
+      },
+      { name: "Material UI", src: "https://v4.mui.com/static/logo.png" },
+    ],
+    sourceCode: "https://github.com/Bereky/easy_UIs/tree/main/one",
+    externalLink: "https://decentral-gray.vercel.app/",
+  },
+  {
+    name: "Github Find",
+    imageSrc: gfind,
+    technologies: [
+      {
+        name: "React",
+        src: "https://cdn-icons-png.flaticon.com/512/3334/3334886.png",
+      },
+      {
+        name: "CSS",
+        src: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+      },
+      { name: "Material UI", src: "https://v4.mui.com/static/logo.png" },
+    ],
+    sourceCode: "https://github.com/Bereky/Github-find",
+    externalLink: "https://eclectic-hamster-b6841d.netlify.app/",
+  },
+  {
+    name: "Password Generator",
+    imageSrc: pwdgenerator,
+    technologies: [
+      {
+        name: "React",
+        src: "https://cdn-icons-png.flaticon.com/512/3334/3334886.png",
+      },
+      {
+        name: "Tailwind CSS",
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png",
+      },
+      { name: "Redux", src: "https://img.icons8.com/color/512/redux.png" },
+    ],
+    sourceCode: "https://github.com/Bereky/password-generator",
+    externalLink: "https://generate-pwd.vercel.app/",
+  },
+  {
+    name: "Amharic Keyboard",
+    imageSrc: keyboard,
+    technologies: [
+      {
+        name: "HTML",
+        src: "https://cdn-icons-png.flaticon.com/512/174/174854.png",
+      },
+      {
+        name: "CSS",
+        src: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+      },
+      {
+        name: "Bootstrap",
+        src: "https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg",
+      },
+    ],
+    sourceCode: "https://github.com/Bereky/Amharic-keyboard",
+    externalLink: "https://remarkable-dodol-8876e4.netlify.app/",
+  },
+];
+
 export default projectConfig;
