97b917b9d474ad71a913044694de3fcb315031b9
unknown <bereketlemma4@gmail.com>
2023-07-16 10:08:26 -0900

    date update on experience list

--- src/components/experience/experienceConfig.js
+++ src/components/experience/experienceConfig.js
@@ -1,24 +1,24 @@
 const experienceConfig = [
-    {
-        jobTitle: 'Web Developer',
-        companyName: 'CariKture India',
-        date: '(Oct 2022 - present)'
-    },
-    {
-        jobTitle: 'Web & Mobile Development Intern',
-        companyName: 'The Sparks Foundation',
-        date: '(Nov 2022 - Dec 2022)'
-    },
-    {
-        jobTitle: 'Front End Developer Freelancer',
-        companyName: 'Upwork',
-        date: '(Aug 2022 - Present)'
-    },
-    {
-        jobTitle: 'Web Development Intern',
-        companyName: 'Lets Grow More',
-        date: '(Nov 2022 - Dec 2022)'
-    },
-]
+  {
+    jobTitle: "Full stack Developer",
+    companyName: "Upwork",
+    date: "(Aug 2022 - Present)",
+  },
+  {
+    jobTitle: "Web Developer Intern",
+    companyName: "CariKture India",
+    date: "(Oct 2022 - De 2022)",
+  },
+  {
+    jobTitle: "Web & Mobile Development Intern",
+    companyName: "The Sparks Foundation",
+    date: "(Nov 2022 - Dec 2022)",
+  },
+  {
+    jobTitle: "Web Development Intern",
+    companyName: "Lets Grow More",
+    date: "(Nov 2022 - Dec 2022)",
+  },
+];
 
 export default experienceConfig;
