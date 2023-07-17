1ab8b5b7fcf336fe41d2b8a851690c95d7c22bb1
unknown <bereketlemma4@gmail.com>
2023-07-17 11:07:37 -0900

    updated title

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,18 +1,25 @@
 import ParticleBg from "./particle/ParticleBg";
 
 const Hero = () => {
-    return ( 
-        <div className="w-full h-screen flex justify-center items-center">
-                <ParticleBg />
-                <div className="w-full h-96 flex justify-center items-center cursor-default">
-                    <div className="w-full h-auto flex justify-center items-center flex-col z-10 space-y-3">
-                        <h1 className="h-auto md:text-7xl  text-6xl font-extrabold text-center ">BEREKET LEMMA</h1> 
-                        <h1 className="h-auto md:text-7xl text-6xl font-medium text-center text-mainBlue"> SOFTWARE DEVELOPER</h1>
-                        <h1 className="h-auto md:text-5xl text-5xl font-medium text-center">BASED IN ETHIOPIA</h1>
-                    </div>
-                </div>
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <ParticleBg />
+      <div className="w-full h-96 flex justify-center items-center cursor-default">
+        <div className="w-full h-auto flex justify-center items-center flex-col z-10 space-y-3">
+          <h1 className="h-auto md:text-7xl  text-6xl font-extrabold text-center ">
+            BEREKET LEMMA
+          </h1>
+          <h1 className="h-auto md:text-7xl text-6xl font-medium text-center text-mainBlue">
+            {" "}
+            SOFTWARE ENGINEER
+          </h1>
+          <h1 className="h-auto md:text-5xl text-5xl font-medium text-center">
+            BASED IN ETHIOPIA
+          </h1>
         </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Hero;
